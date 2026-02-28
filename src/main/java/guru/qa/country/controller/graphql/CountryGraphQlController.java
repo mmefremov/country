2230b226951ab629ba359cf1080fdc587d0ad004
Maksim Efremov <hungryas@gmail.com>
2026-02-28 14:24:51 +0300

    Revert "feat: remove redundant code"
    This reverts commit 01374bbce0fa0ac78a1e4caffff5e70767153de2.

--- src/main/java/guru/qa/country/controller/graphql/CountryGraphQlController.java
+++ src/main/java/guru/qa/country/controller/graphql/CountryGraphQlController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class CountryController {
+public class CountryGraphQlController {
 
     private final CountryService countryService;
 
